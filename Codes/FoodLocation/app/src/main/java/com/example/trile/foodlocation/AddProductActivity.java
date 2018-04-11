aeb39c9dde4728fcbd44fc629d76b80f09a8fdc6
Nguyễn Minh Trí <33915868+minhtri1997@users.noreply.github.com>
2018-04-11 16:00:42 +0700

    Edit,Add Product ,Manager
    Interface :
    + Edit product
    + Add product
    + Manager product

---
+++ Codes/FoodLocation/app/src/main/java/com/example/trile/foodlocation/AddProductActivity.java
@@ -0,0 +1,13 @@
+package com.example.trile.foodlocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_add_product_layout);
+    }
+}
---
+++ Codes/FoodLocation/app/src/main/java/com/example/trile/foodlocation/EditProductActivity.java
@@ -0,0 +1,13 @@
+package com.example.trile.foodlocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_edit_product_layout);
+    }
+}
