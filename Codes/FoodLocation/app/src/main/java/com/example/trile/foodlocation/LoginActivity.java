e56e3ed8fe4911f7893cefcb8d91c77504df066c
Lê Minh Trí <leminhtri340@gmail.com>
2018-05-16 23:01:31 +0700

    Update animation slideshow
    - Slide show
    - Animation background login

--- Codes/FoodLocation/app/src/main/java/com/example/trile/foodlocation/LoginActivity.java
+++ Codes/FoodLocation/app/src/main/java/com/example/trile/foodlocation/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.trile.foodlocation;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import com.example.trile.foodlocation.Adapter.AdapterTabLogin;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,12 +15,21 @@ public class LoginActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private FirebaseAuth mAuth;
+    // ANIMATION BACKGROUND
+    FrameLayout frameLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         mAuth = FirebaseAuth.getInstance();
+
+        frameLayout = (FrameLayout) findViewById(R.id.frame);
+        animationDrawable = (AnimationDrawable)frameLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
 
         mViewPager = (ViewPager) findViewById(R.id.viewPagerLogin);
         mViewPager.setAdapter(new AdapterTabLogin(getSupportFragmentManager()));
